a298a0e43ef93b3f54aafd4c9dd50c38b43b71c9
Roger <rogeriskrenov@gmail.com>
2024-12-03 16:24:58 +0100

    turning this into a tic tac toe game

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from flask import Flask
-import os
-
-
-port = int(os.environ.get('PORT'))
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return 'Deployed!'
+# Game state
+board = [["" for _ in range(3)] for _ in range(3)]
+current_player = "X"
+
+def check_winner():
+    # Check rows and columns
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != "":
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != "":
+            return board[0][i]
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != "":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != "":
+        return board[0][2]
+    # Check for draw
+    if all(board[i][j] != "" for i in range(3) for j in range(3)):
+        return "Draw"
+    return None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/play', methods=['POST'])
+def play():
+    global board, current_player
+    data = request.json
+    row, col = data['row'], data['col']
+
+    if board[row][col] == "":
+        board[row][col] = current_player
+        winner = check_winner()
+        if winner:
+            return jsonify({"winner": winner})
+        current_player = "O" if current_player == "X" else "X"
+        return jsonify({"winner": None})
+    return jsonify({"error": "Invalid move"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
