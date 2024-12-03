334a02a22d2200bcd9a4f97a9f6e7acf6be1c7c4
RogerIsk <rogeriskrenov@gmail.com>
2024-12-03 10:59:05 +0100

    commit 2, port stuff

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import os
+
+
+port = int(os.environ.get('PORT'))
 
 app = Flask(__name__)
 
@@ -6,5 +10,5 @@ app = Flask(__name__)
 def home():
     return 'Deployed!'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=port)
